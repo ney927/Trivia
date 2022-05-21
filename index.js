13a65dc39d56753aeb7236e132e8a24f0148d6c8
ney927 <nbilling9@yahoo.com>
2022-05-21 12:41:27 -0400

    trivia without stats or profiles

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const express = require(express);
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const express = require('express');
 const app = express();
 
 app.listen(3000);
 app.use('/', express.static('public'));
+app.use(express.json());
+
+let score = 0;
+app.post('/score', function(req, res){
+  score += req.body.ans;
+  res.json({'score': score});
+  res.end();
+});
+
+app.post('/profile', function(req, res){
+  console.log(req.body);
+  // if (req.body.score > await db.collection('profiles').get().highscore){
+  //   //high score = req.body.score
+  // }
+})
+
+app.get('/stats', function(req, res){
+  res.json({'score': score});
+  score = 0;
+  res.end();
+});
+
+app.get('*', function(req, res){
+  res.sendFile(process.cwd()+'/public/404.html');
+});
+
