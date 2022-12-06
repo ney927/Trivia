775e0c20ddc02feb2fa714b179611918ac39f7d6
ney927 <nbilling9@yahoo.com>
2022-12-06 11:25:22 -0500

    updated file paths

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const require = createRequire(import.meta.url);
 const express = require('express');
 const app = express();
 
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.listen(3000);
 app.use('/', express.static('public'));
 //to get POST request body
@@ -18,7 +23,7 @@ const encodeUrl = parseUrl.urlencoded({ extended: false })
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 const admin = require("firebase-admin");
-const serviceAccount = require("/Users/neyhabilling/Desktop/VS/JS/node/trivia/secret.json");
+var serviceAccount = require(__dirname+"/secret.json");
 const firebaseConfig = {
   apiKey: "AIzaSyDM3u7JYwnDL9zl76VtzKd-5PyAlUBoYUQ",
   authDomain: "trivia-2e4ad.firebaseapp.com",
@@ -37,59 +42,61 @@ initializeApp(firebaseConfig);
 const db = getFirestore();
 
 let score = 2;
-app.post('/score', function(req, res){
+app.post('/score', function (req, res) {
   score += req.body.ans;
-  res.json({'score': score, 'name': username});
+  res.json({ 'score': score, 'name': username });
   res.end();
 });
 
 let username = 'anon';
-app.post('/newprofile', encodeUrl, async function(req, res){
-  const dbData = {
-    name: req.body.name,
-    highscore: 0
+app.post('/newprofile', encodeUrl, async function (req, res) {
+  console.log(req.body.name);
+  if (req.body.name.trim() == '') {
+    res.sendFile(process.cwd() + '/public/new_profile.html');
+  } else {
+    const dbData = {
+      name: req.body.name,
+      highscore: 0
+    }
+    const ref = await db.collection('profiles').add(dbData);
+    username = req.body.name;
+    res.sendFile(process.cwd() + '/public/question.html');
   }
-  const ref = await db.collection('profiles').add(dbData);
-  username = req.body.name;
-  res.sendFile(process.cwd()+'/public/question.html');
 });
 
-app.post('/start', function(req, res){
+app.post('/start', function (req, res) {
   username = req.body.name;
   res.end();
 });
 
 const path = require('path');
-app.get('/incorrect/:ans', function(req, res){
-  res.sendFile(path.join(process.cwd()+'/public/incorrect.html'));
+app.get('/incorrect/:ans', function (req, res) {
+  res.sendFile(path.join(process.cwd() + '/public/incorrect.html'));
   // console.log(process.cwd()+'/public/incorrect.html');
   // res.redirect('/incorrect.html');
 });
 
-app.get('/allprofiles', async function(req, res){
+app.get('/allprofiles', async function (req, res) {
   const docs = await db.collection('profiles').get();
   let profiles = [];
   docs.forEach(doc => {
     profiles.push(doc.data().name);
   });
-  res.json({profs: profiles});
+  res.json({ profs: profiles });
   res.end();
 });
 
 import { doc, updateDoc } from "firebase/firestore";
-app.get('/stats', async function(req, res){
+app.get('/stats', async function (req, res) {
   const prof = await db.collection('profiles').where('name', '==', username).get();
   let hscore = '';
-  prof.forEach(doc=>{
+  prof.forEach(doc => {
     hscore = doc._fieldsProto.highscore.integerValue;
-    if (score > hscore){
+    if (score > hscore) {
       const ref = db.collection('profiles').doc(username);
-      // const ref = doc(db, 'profiles', username);
-      // updateScore(ref, score);
       hscore = score;
     };
 
-    console.log(doc._fieldsProto.highscore.integerValue, hscore, username);
   });
   res.json({
     'score': score,
@@ -100,7 +107,7 @@ app.get('/stats', async function(req, res){
   res.end();
 });
 
-async function updateScore(ref, newScore){
+async function updateScore(ref, newScore) {
   // const ref = await db.collection('profiles').doc(name).update({highscore: newScore});
   // const ref = await db.collection('profiles').doc(name);
   // const ref = await doc.ref.update({highscore: newScore});
@@ -109,7 +116,7 @@ async function updateScore(ref, newScore){
   console.log(ref);
 }
 
-app.get('*', function(req, res){ 
-  res.sendFile(process.cwd()+'/public/404.html');
+app.get('*', function (req, res) {
+  res.sendFile(process.cwd() + '/public/404.html');
 });
 
